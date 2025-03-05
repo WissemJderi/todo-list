ef269f2c95bf26db47f1a056b8a865574c13237d
Wissem Jderi <asqr43641@gmail.com>
2025-03-05 17:52:38 +0100

    add task editing and deletion functionality; update button layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,52 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [todo, setTodo] = useState([]);
+  const [todo, setTodo] = useState(["Read Quran", "Read Hadiths", "Read Fiqh"]);
   const [currentTask, setCurrentTask] = useState();
   function addTask() {
     setTodo((prevArr) => [...prevArr, currentTask]);
   }
-  const todos = todo.map((task) => {
+
+  function deleteTask(i) {
+    if (todo.length > 1) {
+      if (i == 0) {
+        setTodo(todo.slice(1));
+      } else {
+        setTodo(todo.splice(i));
+      }
+    } else {
+      setTodo([]);
+    }
+  }
+
+  function editTask(i) {
+    let taskEdited = prompt("You Can Edit this Task", todo[i]);
+    if (taskEdited !== null) {
+      setTodo((prevTodo) =>
+        prevTodo.map((task, index) => (index === i ? taskEdited : task))
+      );
+    }
+  }
+  const todos = todo.map((task, i) => {
     return (
       <div className="task" key={(task, Math.random() * 10)}>
         <p>{task}</p>
-        <button>Delete</button>
-        <button>Edit</button>
+        <div className="btns">
+          <button
+            onClick={() => {
+              deleteTask(i);
+            }}
+          >
+            Delete
+          </button>
+          <button
+            onClick={() => {
+              editTask(i);
+            }}
+          >
+            Edit
+          </button>
+        </div>
       </div>
     );
   });
